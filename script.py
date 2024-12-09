99789c968845936099dea12118b1e451bf88b10d
Jesus-Tendicon <jesus.correa@tendicon.com>
2024-12-09 15:00:42 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
-# script.py
 from datetime import datetime
+import webbrowser
 
 def main():
     now = datetime.now()
     print(f"Script ejecutado en: {now}")
+    
+    # Abrir la página de Google
+    webbrowser.open('https://www.google.com')
 
 if __name__ == "__main__":
     main()
